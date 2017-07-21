e6900f8ed55859cd2b7d3f0407a46c6b5346a2bf
Vinit Soni <vinit@tdlab.io>
2017-07-20 21:58:51 -0400

    add SplashScreenActivity

---
+++ app/src/main/java/com/inos/megamarket/ui/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.inos.megamarket.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
